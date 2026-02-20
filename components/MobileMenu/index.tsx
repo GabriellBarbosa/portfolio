bfebe2b749833c6eca512aeb4390a98043fd694e
GabriellBarbosa <bielbarbosa900@gmail.com>
2026-02-20 12:04:19 -0300

    fix: Menu mobile accessibility issue

--- components/MobileMenu/index.tsx
+++ components/MobileMenu/index.tsx
@@ -5,6 +5,7 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
@@ -24,10 +25,12 @@ export function MobileMenu({
       </SheetTrigger>
 
       <SheetContent
-        className="pl-4 space-y-4 pt-[40px] text-xl w-72 bg-white text-zinc-900"
+        className="pl-4 space-y-4 pt-[20px] text-xl w-72 bg-white text-zinc-900"
         side="right"
         onCloseAutoFocus={(e) => e.preventDefault()}
       >
+        <SheetTitle>Seções da página</SheetTitle>
+
         {links.map(({ to, label }, index) => (
           <SheetClose asChild key={index}>
             <Link className="block" href={to}>
